9b72ab1f0da3ef17b2a6b95e232c3af1a0e018a3
Bramcals <bramcals@gmail.com>
2022-10-30 14:04:27 +0100

    Create two simple models

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band, Title
 
 
 def hello(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
+    bands = Band.objects.all()
+    titles = Title.objects.all()
+    return HttpResponse(
+        f"""
+        <h1>Hello Django!</h1>
+        <p>My favorite bands are:<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+            <li>Vet he Thies: {titles[0].band_name} - Title: {titles[0].title}</li>
+        </ul>
+    """
+    )
 
 
 def about(request):
