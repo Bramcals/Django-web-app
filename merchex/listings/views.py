249b8e0a7233a7501300943284f1236135a4e728
Bramcals <bramcals@gmail.com>
2022-10-31 08:33:47 +0100

    Implemented templates

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,19 +5,7 @@ from listings.models import Band, Title
 
 def hello(request):
     bands = Band.objects.all()
-    titles = Title.objects.all()
-    return HttpResponse(
-        f"""
-        <h1>Hello Django!</h1>
-        <p>My favorite bands are:<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-            <li>Vet he Thies: {titles[0].band_name} - Title: {titles[0].title}</li>
-        </ul>
-    """
-    )
+    return render(request, "listings/hello.html", {"bands": bands})
 
 
 def about(request):
